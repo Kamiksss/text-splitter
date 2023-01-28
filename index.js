2e2c6b5f85a181bb035cb062ee9a53a817c75877
Kamiksss <blue.kamlotov@gmail.com>
2023-01-28 22:48:47 +0100

    backend

---
+++ index.js
@@ -0,0 +1,15 @@
+//working backend
+
+const prompt = require("prompt-sync")();
+const fs = require("fs");
+const myConsole = new console.Console(fs.createWriteStream("./output.txt"));
+
+var input = prompt(">>");
+
+podzielone = input.match(/.[\s\S]{1,1999}/g);
+
+const n = podzielone.length;
+
+for (let i = 0; i < n; i++) {
+  myConsole.log(podzielone[i]);
+}
